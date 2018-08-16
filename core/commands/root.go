b46e3d24f75d1ba9ca7f97a424653b69e930af14
Kevin Atkinson <k@kevina.org>
2018-08-15 20:02:18 -0400

    Provide new "cid" sub-command.
    License: MIT
    Signed-off-by: Kevin Atkinson <k@kevina.org>

--- core/commands/root.go
+++ core/commands/root.go
@@ -67,6 +67,7 @@ TOOL COMMANDS
   version       Show ipfs version information
   update        Download and apply go-ipfs updates
   commands      List all available commands
+  cid           Convert and discover properties of CIDs
 
 Use 'ipfs <command> --help' to learn more about each command.
 
@@ -139,6 +140,7 @@ var rootSubcommands = map[string]*cmds.Command{
 	"urlstore":  urlStoreCmd,
 	"version":   lgc.NewCommand(VersionCmd),
 	"shutdown":  daemonShutdownCmd,
+	"cid":       CidCmd,
 }
 
 // RootRO is the readonly version of Root
